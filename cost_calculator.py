1352fb01c1e773a9b58497683904375f73c456bf
Andrew Reynolds <andrewreynolds@Andrews-MacBook-Air-3.local>
2023-04-23 18:03:45 -0700

    created cost_finder function

--- cost_calculator.py
+++ cost_calculator.py
@@ -2,12 +2,11 @@ import cost_finder as cf
 import cost_dictionaries as cd
 
 
-print(cf.cost_finder(cd.ground_shipping_dict, cd.ground_shipping_premium, cd.drone_shipping_dict, 15))
 
 def cost_finder():
     inputed_weight = float(input("What is the weight of your package? "))
-    return inputed_weight
+    shipping_cost_dict = cf.cost_finder(cd.ground_shipping_dict, cd.ground_shipping_premium, cd.drone_shipping_dict, inputed_weight)
+    return shipping_cost_dict
 
 
-
-print(type(cost_finder()))
+print(cost_finder())
