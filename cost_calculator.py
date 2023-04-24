262b9994285b8f4a038d961d70c1dfb4888858d8
Andrew <andrew_reynolds@mac.com>
2023-04-23 18:15:10 -0700

    updating cost_finder function

--- cost_calculator.py
+++ cost_calculator.py
@@ -6,6 +6,13 @@ import cost_dictionaries as cd
 def cost_finder():
     inputed_weight = float(input("What is the weight of your package? "))
     shipping_cost_dict = cf.cost_finder(cd.ground_shipping_dict, cd.ground_shipping_premium, cd.drone_shipping_dict, inputed_weight)
+    cheapest_option = ""
+    cheapest_price = 0
+    
+    for key, value in shipping_cost_dict.items():
+        print(value)
+    
+    
     return shipping_cost_dict
 
 
